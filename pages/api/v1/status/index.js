810bca2c0dc939e393deeb16b23fdb799340bec7
Ramon Cunha <ramon.network@hotmail.com>
2024-12-04 23:53:20 -0300

    add updated_at test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,14 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  // const result = await database.query("SELECT 1 + 1;");
-  // console.log(result);
-  response.status(200).json({ chave: "sou uma pessoa acima da média" });
+  const updateAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updateAt,
+    version: "",
+    max_connections: "",
+    used_connections: "",
+  });
 }
 
 export default status;
