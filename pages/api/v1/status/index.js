0db6eeadaccf5a7b642799e6ae5f789c3f2ba5bd
Ramon Cunha <ramon.network@hotmail.com>
2024-12-09 20:54:40 -0300

    finish /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,12 +2,24 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
+  const dbVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
 
   response.status(200).json({
     updated_at: updateAt,
-    version: "",
-    max_connections: "",
-    used_connections: "",
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: parseInt(usedConnections.rows[0].count),
+      },
+    },
   });
 }
 
