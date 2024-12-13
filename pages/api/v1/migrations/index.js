50171853624aef8d63a5e580cdb0108e021b0cb1
Ramon Cunha <ramon.network@hotmail.com>
2024-12-12 23:19:29 -0300

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,13 +2,30 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
+  const defaultMigrationOptions = {
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  }
+
+  if (request.method == "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    return response.status(200).json(pendingMigrations);
+  } else if (request.method == "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false
+    });
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end;
 }
