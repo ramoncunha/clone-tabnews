bd60f7d752f569c88d379b1ac1c0942b93126f77
Ramon Cunha <ramon.network@hotmail.com>
2024-12-13 23:57:47 -0300

    add dotenv-expand to configure enviroment variable, refactor database.js

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,24 +1,30 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  }
+  };
 
   if (request.method == "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   } else if (request.method == "POST") {
     const migratedMigrations = await migrationRunner({
       ...defaultMigrationOptions,
-      dryRun: false
+      dryRun: false,
     });
+
+    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
