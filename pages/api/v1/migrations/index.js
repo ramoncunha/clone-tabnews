11b5da4c8d7c37c83630f889dae3f4b6219549a6
Ramon Cunha <ramon.network@hotmail.com>
2024-12-16 21:06:22 -0300

    corrige retorno do /api/v1/migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -14,8 +14,6 @@ export default async function migrations(request, response) {
     migrationsTable: "pgmigrations",
   };
 
-  console.log(request);
-
   if (request.method == "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
     await dbClient.end();
@@ -35,5 +33,5 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
-  return response.status(405).end;
+  return response.status(405).end();
 }
