827f911a235c33e483a2eac865308f9b157b3d48
Ramon Cunha <ramon.network@hotmail.com>
2024-12-27 19:29:39 -0300

    fix `prettier` style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,11 +4,11 @@ async function status(request, response) {
   const updateAt = new Date().toISOString();
   const dbVersion = await database.query("SHOW server_version;");
   const maxConnections = await database.query("SHOW max_connections;");
-  
+
   const databaseName = process.env.POSTGRES_DB;
   const usedConnections = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-    values: [databaseName]
+    values: [databaseName],
   });
 
   response.status(200).json({
